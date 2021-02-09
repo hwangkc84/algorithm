1a4d4de9d0731f46b0d0e6324af01277dd23fdd1
kchwang <hwangkc84@gmail.com>
2021-02-10 01:22:11 +0900

    job

---
+++ Job.java
@@ -0,0 +1,21 @@
+
+public class Job implements Comparable<Job>{
+    int start;
+    int workTime;
+
+    public Job(int start , int time){
+        this.start = start;
+        this.workTime = time;
+    }
+
+	@Override
+	public int compareTo(Job o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+  
+    
+
+
+}
--- Node.java
+++ Node.java
@@ -1,21 +1,31 @@
+import java.util.*;
 import java.util.PriorityQueue;
+
 
 class Node{
 
     public static void main(String[] args) {
+        Node n = new Node();
 
-        int[] arr = {1,2,3,9,10,12};
+       /*  int[] arr = {1,2,3,9,10,12};
         int k = 7;
 
         Node n = new Node();
-        System.out.println(n.solution(arr, k));
+        System.out.println(n.solution(arr, k)); */
+
+        int [][] jobs = {{0,3},{1,9},{2,6}};
+        Arrays.sort(jobs,);
+        n.solution(jobs);
     }
 
     //디스크 컨트롤러
     public int solution(int[][] jobs) {
         int answer = 0;
+        int len = jobs.length;
 
-        
+        for(int i=0; i<len; i++){
+            System.out.println(jobs[0][i]);
+        }
 
 
         return answer;
